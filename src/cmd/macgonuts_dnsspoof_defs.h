96bc5d21639960b2fe49e2e0a5d8c227fc42e606
rafael-santiago <voidbrainvoid@tutanota.com>
2023-02-25 11:33:36 -0200

    Move dnspoof routine to macgonuts lib

--- src/cmd/macgonuts_dnsspoof_defs.h
+++ src/cmd/macgonuts_dnsspoof_defs.h
@@ -5,11 +5,11 @@
  * This source code is licensed under the BSD-style license found in the
  * LICENSE file in the root directory of this source tree.
  */
-#ifndef MACGONUTS_DNSSPOOF_DEFS_H
-#define MACGONUTS_DNSSPOOF_DEFS_H 1
+#ifndef MACGONUTS_CMD_MACGONUTS_DNSSPOOF_DEFS_H
+#define MACGONUTS_CMD_MACGONUTS_DNSSPOOF_DEFS_H 1
 
 #define macgonuts_dnsspoof_etc_hoax(s) ((macgonuts_etc_hoax_handle *)(s)->metainfo.arg[0])
 #define macgonuts_dnsspoof_iplist(s) ((macgonuts_iplist_handle *)(s)->metainfo.arg[1])
 #define macgonuts_dnsspoof_ttl(s) (*(uint32_t *)(s)->metainfo.arg[2])
 
-#endif // MACGONUTS_DNSSPOOF_DEFS_H
+#endif // MACGONUTS_CMD_MACGONUTS_DNSSPOOF_DEFS_H
---
+++ src/macgonuts_dnsspoof.h
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2023, Rafael Santiago
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+#ifndef MACGONUTS_DNSSPOOF_H
+#define MACGONUTS_DNSSPOOF_H 1
+
+#include <macgonuts_types.h>
+#include <macgonuts_iplist.h>
+#include <macgonuts_etc_hoax.h>
+
+int macgonuts_dnsspoof(const macgonuts_socket_t rsk, struct macgonuts_spoof_layers_ctx *spf_layers,
+                       macgonuts_iplist_handle *iplist_handle,
+                       macgonuts_etc_hoax_handle *etc_hoax,
+                       const uint32_t dns_answer_ttl,
+                       const unsigned char *ethfrm, const size_t ethfrm_size);
+
+#endif // MACGONUTS_DNSSPOOF_H
